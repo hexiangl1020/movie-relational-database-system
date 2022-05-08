ef54025b8dbe3c6514051b55744327346781039d
Jingyi Wu <jingywu@seas.upenn.edu>
2022-05-08 17:45:33 -0400

    mod style

--- client/src/fetcher.js
+++ client/src/fetcher.js
@@ -1,110 +1,141 @@
-import config from './config.json'
+import config from './config.json';
 const getcharacterMbtiList = async (value) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/characterMbtiList?mbti=${value}`, {
-        method: 'GET',
-    })
-    return res.json()
-}
+  var res = await fetch(
+    `http://${config.server_host}:${config.server_port}/characterMbtiList?mbti=${value}`,
+    {
+      method: 'GET',
+    }
+  );
+  return res.json();
+};
 
-const getmbtiMatches = async (page,pagesize,value) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/mbti_matches/${value}`, {
-        method: 'GET',
-    })
-    return res.json()
-}
+const getmbtiMatches = async (page, pagesize, value) => {
+  var res = await fetch(
+    `http://${config.server_host}:${config.server_port}/mbti_matches/${value}`,
+    {
+      method: 'GET',
+    }
+  );
+  return res.json();
+};
 
 const gettop5mvmbti = async (value) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/top5mvmbti?mbti=${value}`, {
-        method: 'GET',
-    })
-    return res.json()
-}
+  var res = await fetch(
+    `http://${config.server_host}:${config.server_port}/top5mvmbti?mbti=${value}`,
+    {
+      method: 'GET',
+    }
+  );
+  return res.json();
+};
 
 const getmvmatches = async (value) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/movie/${value}`, {
-        method: 'GET',
-    })
-    return res.json()
-}
+  var res = await fetch(
+    `http://${config.server_host}:${config.server_port}/movie/${value}`,
+    {
+      method: 'GET',
+    }
+  );
+  return res.json();
+};
 
 const getmvmbtipct = async (value) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/mvpct/${value}`, {
-        method: 'GET',
-    })
-    return res.json()
-}
+  var res = await fetch(
+    `http://${config.server_host}:${config.server_port}/mvpct/${value}`,
+    {
+      method: 'GET',
+    }
+  );
+  return res.json();
+};
 
 const getCharacter = async (movieId, name) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/character/${movieId}/${name}`, {
-        method: 'GET',
-    })
-    return res.json()
-}
+  var res = await fetch(
+    `http://${config.server_host}:${config.server_port}/character/${movieId}/${name}`,
+    {
+      method: 'GET',
+    }
+  );
+  return res.json();
+};
 
 const getMovie = async (movieId) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/movie/${movieId}`, {
-        method: 'GET',
-    })
-    return res.json()
-}
+  var res = await fetch(
+    `http://${config.server_host}:${config.server_port}/movie/${movieId}`,
+    {
+      method: 'GET',
+    }
+  );
+  return res.json();
+};
 
-const getCharacterSameMBTI= async (movieId,name) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/findcsametype?mid=${movieId}&cname=${name}`, {
-        method: 'GET',
-    })
-    return res.json()
-}
+const getCharacterSameMBTI = async (movieId, name) => {
+  var res = await fetch(
+    `http://${config.server_host}:${config.server_port}/findcsametype?mid=${movieId}&cname=${name}`,
+    {
+      method: 'GET',
+    }
+  );
+  return res.json();
+};
 
-const getCharacterSameActorMBTI= async (movieId,name) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/samembtiactor?mvid=${movieId}&name=${name}`, {
-        method: 'GET',
-    })
-    return res.json()
-}
+const getCharacterSameActorMBTI = async (movieId, name) => {
+  var res = await fetch(
+    `http://${config.server_host}:${config.server_port}/samembtiactor?mvid=${movieId}&name=${name}`,
+    {
+      method: 'GET',
+    }
+  );
+  return res.json();
+};
 
 const rankbymbti = async (value) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/actormbtiplayed`, {
-        method: 'GET',
-    })
-    return res.json()
-}
+  var res = await fetch(
+    `http://${config.server_host}:${config.server_port}/actormbtiplayed`,
+    {
+      method: 'GET',
+    }
+  );
+  return res.json();
+};
 
 const actorpct = async (value) => {
-    console.log(value)
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/actorpct/${value}`, {
-        method: 'GET',
-    })
-    return res.json()
-}
+  console.log(value);
+  var res = await fetch(
+    `http://${config.server_host}:${config.server_port}/actorpct/${value}`,
+    {
+      method: 'GET',
+    }
+  );
+  return res.json();
+};
 const getAllMovies = async (
-    title,
-    startYearLow,
-    startYearHigh,
-    RatingLow,
-    RatingHigh
-  ) => {
-    var res = await fetch(
-      `http://${config.server_host}:${config.server_port}/movieList?title=${title}&startYearLow=${startYearLow}&startYearHigh=${startYearHigh}&RatingLow=${RatingLow}&RatingHigh=${RatingHigh}`,
-      {
-        method: 'GET',
-      }
-    );
-    return res.json();
-  }
-
-
+  title,
+  startYearLow,
+  startYearHigh,
+  RatingLow,
+  RatingHigh
+) => {
+  var res = await fetch(
+    `http://${config.server_host}:${config.server_port}/movieList?title=${title}&startYearLow=${startYearLow}&startYearHigh=${startYearHigh}&RatingLow=${RatingLow}&RatingHigh=${RatingHigh}`,
+    {
+      method: 'GET',
+    }
+  );
+  return res.json();
+};
 
 export {
-    getcharacterMbtiList,
-    getmbtiMatches,
-    gettop5mvmbti,
-    getmvmatches,
-    getmvmbtipct,
-    getCharacter,
-    getMovie,
-    getCharacterSameMBTI,
-    getCharacterSameActorMBTI,
-    rankbymbti,
-    actorpct,
-    getAllMovies
-}
+  getcharacterMbtiList,
+  getmbtiMatches,
+  gettop5mvmbti,
+  getmvmatches,
+  getmvmbtipct,
+  getCharacter,
+  getMovie,
+  getCharacterSameMBTI,
+  getCharacterSameActorMBTI,
+  rankbymbti,
+  actorpct,
+  getAllMovies,
+};
--- client/src/pages/Movieprofile.js
+++ client/src/pages/Movieprofile.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import { withRouter } from "react-router";
-import { Form, FormInput, FormGroup, Button, Card, CardBody, CardTitle, Progress } from "shards-react";
+import { withRouter } from 'react-router';
 import {
-    Table,
-    Pagination,
-    Select,
-    Row,
-    Col,
-    Divider,
-    Slider,
-    Rate 
-} from 'antd'
+  Form,
+  FormInput,
+  FormGroup,
+  Button,
+  Card,
+  CardBody,
+  CardTitle,
+  Progress,
+} from 'shards-react';
+import {
+  Table,
+  Pagination,
+  Select,
+  Row,
+  Col,
+  Divider,
+  Slider,
+  Rate,
+} from 'antd';
 import { RadarChart } from 'react-vis';
 import { format } from 'd3-format';
 
 import MenuBar from '../components/MenuBar';
-import { getmvmatches,getmvmbtipct} from '../fetcher';
+import { getmvmatches, getmvmbtipct } from '../fetcher';
 const { Column, ColumnGroup } = Table;
 const { Option } = Select;
 
@@ -24,7 +33,7 @@ const matchmovieColumns = [
     title: 'movie_id',
     dataIndex: 'movie_id',
     key: 'movie_id',
-    
+
     //render: (text, row) => <a href={`/players?id=${row.PlayerId}`}>{text}</a>,
   },
   {
@@ -36,14 +45,12 @@ const matchmovieColumns = [
     title: 'startYear',
     dataIndex: 'startYear',
     key: 'startYear',
-    
   },
   {
     title: 'averageRating',
     dataIndex: 'averageRating',
     key: 'averageRating',
-  
-  }
+  },
 ];
 
 const moviepctColumns = [
@@ -63,7 +70,7 @@ const moviepctColumns = [
     dataIndex: 'percentage',
     key: 'percentage',
     sorter: (a, b) => a.percentage - b.percentage,
-  }
+  },
 ];
 
 class MoviePage extends React.Component {
@@ -72,45 +79,41 @@ class MoviePage extends React.Component {
     this.state = {
       PageNumber: 1,
       PageSize: 10,
-      movieResults:[],
-      movie_idQuery:'',
-      pctresults:[]
+      movieResults: [],
+      movie_idQuery: '',
+      pctresults: [],
     };
-    this.handleQueryChange = this.handleQueryChange.bind(this)
-    this.updateSearchResults = this.updateSearchResults.bind(this)
+    this.handleQueryChange = this.handleQueryChange.bind(this);
+    this.updateSearchResults = this.updateSearchResults.bind(this);
     //this.movieOnChange=this.movieOnChange.bind(this)
-
   }
 
   handleQueryChange(event) {
-    this.setState({ movie_idQuery: event.target.value })
+    this.setState({ movie_idQuery: event.target.value });
   }
 
   updateSearchResults() {
-
     //TASK 23: call getPlayerSearch and update playerResults in state. See componentDidMount() for a hint
-    getmvmatches(this.state.movie_idQuery).then(res => {
-        this.setState({ movieResults: res.results })
-    })
+    getmvmatches(this.state.movie_idQuery).then((res) => {
+      this.setState({ movieResults: res.results });
+    });
 
-    getmvmbtipct(this.state.movie_idQuery).then(res => {
-      this.setState({ pctresults: res.results })
-    })
-
-}
+    getmvmbtipct(this.state.movie_idQuery).then((res) => {
+      this.setState({ pctresults: res.results });
+    });
+  }
 
   componentDidMount() {
     const { mvid } = this.props.match.params;
     this.setState({ movie_idQuery: mvid });
 
-    getmvmatches(mvid).then(res => {
-      console.log(res)
-      this.setState({ movieResults: res.results[0] })
-    })
+    getmvmatches(mvid).then((res) => {
+      this.setState({ movieResults: res.results[0] });
+    });
 
-    getmvmbtipct(mvid).then(res => {
-      this.setState({ pctresults: res.results })
-    })
+    getmvmbtipct(mvid).then((res) => {
+      this.setState({ pctresults: res.results });
+    });
 
     //gettop5mvmbti(null).then(res => {
     //  this.setState({ top5: res.results })
@@ -119,29 +122,31 @@ class MoviePage extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className='movieProfile'>
         <MenuBar />
-          <div style={{ width: '70vw', margin: '0 auto', marginTop: '2vh' }}>
-            <Card>
-              <CardBody>
-                <Row gutter='30' align='middle' justify='center'>
-                  <Col flex={2} style={{ textAlign: 'left' }}>
-                    <h3>{this.state.movieResults.primaryTitle}</h3>
-                  </Col>
-                </Row>
-                <Row gutter='30' align='middle' justify='left'>
-                  <Col>
-                    <h5>Start Year: {this.state.movieResults.startYear}</h5>
-                  </Col>
-                </Row>
-                <Row gutter='30' align='middle' justify='left'>
-                  <Col>
-                    <h5>AverageRating: {this.state.movieResults.averageRating}</h5>
-                  </Col>
-                </Row>
-              </CardBody> 
-            </Card>
-          </div>
+        <div style={{ width: '70vw', margin: '0 auto', marginTop: '2vh' }}>
+          <Card>
+            <CardBody>
+              <Row gutter='30' align='middle' justify='center'>
+                <Col flex={2} style={{ textAlign: 'left' }}>
+                  <h3>{this.state.movieResults.primaryTitle}</h3>
+                </Col>
+              </Row>
+              <Row gutter='30' align='middle' justify='left'>
+                <Col>
+                  <h5>Start Year: {this.state.movieResults.startYear}</h5>
+                </Col>
+              </Row>
+              <Row gutter='30' align='middle' justify='left'>
+                <Col>
+                  <h5>
+                    AverageRating: {this.state.movieResults.averageRating}
+                  </h5>
+                </Col>
+              </Row>
+            </CardBody>
+          </Card>
+        </div>
         <Divider />
         <div style={{ width: '70vw', margin: '0 auto', marginTop: '5vh' }}>
           <h3>Percentage of Each MBTI type of characters in this movie</h3>
@@ -154,8 +159,7 @@ class MoviePage extends React.Component {
               showQuickJumper: true,
             }}
           />
-          </div>
-
+        </div>
       </div>
     );
   }
